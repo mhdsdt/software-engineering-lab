71b6b894bd9baf4879fd81849211ba4dad7170a4
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2024-07-08 08:31:10 +0330

    ci: update workflow of experiment-1 for production env

--- experiment-1-git/vite.config.js
+++ experiment-1-git/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
+const base = process.env.DEPLOY_ENV === 'staging' ? '/se-lab-1-staging/' : '/se-lab-1/'
+
 export default defineConfig({
-  server: {
-    hmr: true
-  }
+  base
 })
