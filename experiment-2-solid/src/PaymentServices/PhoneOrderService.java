5a5e0a0065bc52f5cad45e7524727d9c02684488
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2024-07-19 17:27:44 +0330

    feat: add phone method to OrderService and implement it with PhoneOrderService

--- experiment-2-solid/src/PaymentServices/PhoneOrderService.java
+++ experiment-2-solid/src/PaymentServices/PhoneOrderService.java
@@ -1,34 +1,33 @@
 package PaymentServices;
 
-public class OnlineOrderService implements OrderService {
+public class PhoneOrderService implements OrderService {
     @Override
     public void onSiteOrderRegister(String customerName) {
-        //Empty Body!
+        // Empty Body
     }
 
     @Override
     public void onlineOrderRegister(String customerName) {
-        System.out.println("online order registered for " + customerName);
+        // Empty Body
     }
 
     @Override
     public void phoneOrderRegister(String customerName) {
-        //Empty Body
+        System.out.println("Phone order registered for " + customerName);
     }
 
     @Override
     public void onSiteOrderPayment(int foodPrice) {
-        //Empty Body
+        // Empty Body
     }
 
     @Override
     public void onlineOrderPayment(int foodPrice) {
-        System.out.println("online Payment with Price : " + foodPrice + " Tomans!");
+        // Empty Body
     }
 
     @Override
     public void phoneOrderPayment(int foodPrice) {
-        //Empty Body
+        System.out.println("Phone Payment with Price: " + foodPrice + " Tomans!");
     }
-
 }
