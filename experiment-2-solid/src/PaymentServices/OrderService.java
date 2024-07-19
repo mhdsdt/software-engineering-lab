6de21590f85915efe623aedebcb1f77a6543185c
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2024-07-19 17:33:07 +0330

    fix: remove redundant public keyword from OrderService interface

--- experiment-2-solid/src/PaymentServices/OrderService.java
+++ experiment-2-solid/src/PaymentServices/OrderService.java
@@ -1,10 +1,10 @@
 package PaymentServices;
 
 public interface OrderService {
-    public void onSiteOrderRegister(String customerName);
-    public void onlineOrderRegister(String customerName);
-    public void phoneOrderRegister(String customerName);
-    public void onSiteOrderPayment(int foodPrice);
-    public void onlineOrderPayment(int foodPrice);
-    public void phoneOrderPayment(int foodPrice);
+    void onSiteOrderRegister(String customerName);
+    void onlineOrderRegister(String customerName);
+    void phoneOrderRegister(String customerName);
+    void onSiteOrderPayment(int foodPrice);
+    void onlineOrderPayment(int foodPrice);
+    void phoneOrderPayment(int foodPrice);
 }
