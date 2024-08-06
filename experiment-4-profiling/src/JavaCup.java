6701b4cd127d3d0101dff3cd3c76e566074e6ddf
abolism <eshagh.abolfazl@gmail.com>
2024-08-06 20:38:38 +0330

    Primitive Array

--- experiment-4-profiling/src/JavaCup.java
+++ experiment-4-profiling/src/JavaCup.java
@@ -23,11 +23,12 @@ public class JavaCup {
         else { System.out.println("NO"); }
     }
     public static void temp() {
-        ArrayList<Integer> a = new ArrayList<>(200000000);
+        int[] a = new int[200000000];
+        int index = 0;
         for (int i = 0; i < 10000; i++)
         {
             for (int j = 0; j < 20000; j++) {
-                a.add(i + j);
+                a[index++] = i + j;
             }
         }
     }
