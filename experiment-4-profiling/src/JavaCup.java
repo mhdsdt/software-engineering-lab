c23d2b693fb950ee24d2139b2183496dba479d8c
abolism <eshagh.abolfazl@gmail.com>
2024-08-06 21:35:06 +0330

    changing temp to primitve array

--- experiment-4-profiling/src/JavaCup.java
+++ experiment-4-profiling/src/JavaCup.java
@@ -22,11 +22,11 @@ public class JavaCup {
         else { System.out.println("NO"); }
     }
     public static void temp() {
-        ArrayDeque<Integer> a = new ArrayDeque<>(200000000);
-        for (int i = 0; i < 10000; i++)
-        {
+        int[] a = new int[200000000];
+        int index = 0;
+        for (int i = 0; i < 10000; i++) {
             for (int j = 0; j < 20000; j++) {
-                a.add(i + j);
+                a[index++] = i + j;
             }
         }
     }
