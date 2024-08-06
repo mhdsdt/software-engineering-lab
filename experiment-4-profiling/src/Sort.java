aa78546818ed6d3a36f727232ad569ad82057356
abolism <eshagh.abolfazl@gmail.com>
2024-08-06 21:09:26 +0330

    part2, merge sort

--- experiment-4-profiling/src/Sort.java
+++ experiment-4-profiling/src/Sort.java
@@ -1,23 +1,58 @@
 import java.util.Random;
 
 public class Sort {
-    //    using bubble sort (a non-optimal sorting algorithm)
-    public static void bubbleSort(int[] array) {
-        int n = array.length;
-        boolean swapped;
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (array[j] > array[j + 1]) {
-                    // Swap array[j] and array[j + 1]
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                    swapped = true;
-                }
+    //    using merge sort, the most optimal sorting algorithm
+    public static void mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            // Recursively sort the first and second halves
+            mergeSort(array, left, mid);
+            mergeSort(array, mid + 1, right);
+
+            // Merge the sorted halves
+            merge(array, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] array, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        // Copy data to temporary arrays
+        System.arraycopy(array, left, leftArray, 0, n1);
+        System.arraycopy(array, mid + 1, rightArray, 0, n2);
+
+        int i = 0, j = 0;
+        int k = left;
+
+        // Merge the temporary arrays back into the original array
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
             }
-            // If no elements were swapped, the array is already sorted
-            if (!swapped) break;
+            k++;
+        }
+
+        // Copy remaining elements of leftArray, if any
+        while (i < n1) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of rightArray, if any
+        while (j < n2) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
         }
     }
 
@@ -29,18 +64,27 @@ public class Sort {
         for (int i = 0; i < array.length; i++) {
             array[i] = rand.nextInt(100000);
         }
+
+        // Print unsorted array (optional, comment out if too large)
         System.out.println("Unsorted array:");
-        for (int num : array) {
-            System.out.print(num + " ");
+        for (int i = 0; i < Math.min(array.length, 20); i++) { // Print only first 20 elements for brevity
+            System.out.print(array[i] + " ");
         }
         System.out.println();
 
-        bubbleSort(array);
+        // Sort the array using Merge Sort
+        System.out.println("Sorting array using Merge Sort...");
+        long startTime = System.currentTimeMillis();
+        mergeSort(array, 0, array.length - 1);
+        long endTime = System.currentTimeMillis();
 
-        System.out.println("Sorted array using Bubble Sort:");
-        for (int num : array) {
-            System.out.print(num + " ");
+        // Print sorted array (optional, comment out if too large)
+        System.out.println("Sorted array:");
+        for (int i = 0; i < Math.min(array.length, 20); i++) { // Print only first 20 elements for brevity
+            System.out.print(array[i] + " ");
         }
         System.out.println();
+
+        System.out.println("Merge Sort took: " + (endTime - startTime) + " milliseconds");
     }
 }
