3d62351511de7ced46092200d04ae9afbc0b9c3a
abolism <eshagh.abolfazl@gmail.com>
2024-08-06 20:37:42 +0330

    ArrayList with Initial Capacity

--- experiment-4-profiling/src/JavaCup.java
+++ experiment-4-profiling/src/JavaCup.java
@@ -23,7 +23,7 @@ public class JavaCup {
         else { System.out.println("NO"); }
     }
     public static void temp() {
-        ArrayList a = new ArrayList();
+        ArrayList<Integer> a = new ArrayList<>(200000000);
         for (int i = 0; i < 10000; i++)
         {
             for (int j = 0; j < 20000; j++) {
