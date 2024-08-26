7c01e5fb3dbdf216c8a5b5953e9f26dc6d2fc7ae
abolism <eshagh.abolfazl@gmail.com>
2024-08-27 00:58:02 +0330

    Implement Facade for Parsing and Code Generation

--- experiment-6-patterns/src/main/java/MiniJava/Main.java
+++ experiment-6-patterns/src/main/java/MiniJava/Main.java
@@ -1,20 +1,14 @@
 package MiniJava;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
-import MiniJava.errorHandler.ErrorHandler;
-import MiniJava.parser.Parser;
-
+import MiniJava.facade.MiniJavaFacade;
 
 public class Main {
     public static void main(String[] args) {
-        Parser parser = new Parser();
-        try {
-            // start parsing
-            parser.startParse(new Scanner(new File("src/main/resources/code")));
-        } catch (FileNotFoundException e) {
-            ErrorHandler.printError(e.getMessage());
+        if (args.length == 0) {
+            System.out.println("Usage: java Main <source-file>");
+            return;
         }
+        MiniJavaFacade facade = new MiniJavaFacade();
+        facade.initiate(args[0]);
     }
 }
